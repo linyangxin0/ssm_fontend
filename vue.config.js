3f303dd39c1682a89f2c6685ebc4706e9750c0c0
linyangxin <1227143051@qq.com>
2020-05-16 12:18:56 +0800

    first commit

---
+++ vue.config.js
@@ -0,0 +1,20 @@
+module.exports={
+  configureWebpack:{
+    resolve:{
+      alias:{
+        'assets':'@/assets',
+        'common':'@/common',
+        'components':'@/components',
+        'network':'@/network',
+        'views':'@/views',
+      }
+    }
+  },
+
+  chainWebpack: config => {
+    config.module
+      .rule('svg-sprite')
+      .use('svgo-loader')
+      .loader('svgo-loader')
+  }
+}
